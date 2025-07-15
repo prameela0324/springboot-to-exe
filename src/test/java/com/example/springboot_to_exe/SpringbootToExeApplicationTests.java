b308e9e1d2a822ef68ed7eeeb2560caad9e17216
prameela <Prameela.Battineni@Trivium-eSolutions.com>
2025-07-15 11:33:09 +0530

    initial commit

---
+++ src/test/java/com/example/springboot_to_exe/SpringbootToExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_to_exe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
